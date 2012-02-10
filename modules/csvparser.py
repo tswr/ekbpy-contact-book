66f25df7e14a99bd1dd87873ae4d44fd1961eb30
vvn <vvn@ubuntu.(none)>
2012-02-10 17:41:20 +0700

    issue #4 fixed

--- modules/csvparser.py
+++ modules/csvparser.py
@@ -5,7 +5,22 @@ def CSVParse(fileHandle):
     Разбирает csv файл по переданному открытому хендлу.
     Возвращает список словарей. Ключи в словарях - заголовки csv файла, значения - соответствующие заголовкам данные.
     """
-    raise NotImplementedError
+    lines = fileHandle.readlines()
+    lines = map(lambda x: x.strip(), lines)
+
+    headers = lines[0].split(',')
+    
+    out = []	
+    for i in xrange(1, len(lines)):
+        row = lines[i].split(',')
+        out_dict = {}
+        for j in xrange(0, min(len(row), len(headers))):
+            out_dict[headers[j]] = row[j]
+        out.append(out_dict)                        
+   
+    return out
+	
+    #raise NotImplementedError
 
 ##############
 # unit tests #
