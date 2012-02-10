0657aeb18e0bc6067a41db314496d2c769cd389f
vvn <vvn@ubuntu.(none)>
2012-02-10 18:07:21 +0700

    fixed multiline values

--- modules/csvparser.py
+++ modules/csvparser.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
+import csv
+
 
 def CSVParse(fileHandle):
     """
     Разбирает csv файл по переданному открытому хендлу.
     Возвращает список словарей. Ключи в словарях - заголовки csv файла, значения - соответствующие заголовкам данные.
     """
-    lines = fileHandle.readlines()
-    lines = map(lambda x: x.strip(), lines)
-
-    headers = lines[0].split(',')
-    
-    out = []	
-    for i in xrange(1, len(lines)):
-        row = lines[i].split(',')
-        out_dict = {}
-        for j in xrange(0, min(len(row), len(headers))):
-            out_dict[headers[j]] = row[j]
-        out.append(out_dict)                        
+    reader = csv.reader(fileHandle)    
+    headers = None
+    out = []
+	
+    for row in reader:
+        if not headers: 
+            headers = row
+        else:        
+            out_dict = {}
+            for j in xrange(0, min(len(row), len(headers))):
+                out_dict[headers[j]] = row[j]
+            out.append(out_dict)                        
    
     return out
 	
@@ -34,6 +36,14 @@ class CSVParserTestCase(unittest.TestCase):
     def test_simple(self):
         test = "one,two,three\n1,2,3\n4,5,6"
         expectedRes = [{"one":"1","two":"2","three":"3"},{"one":"4","two":"5","three":"6"}]
+        sio = StringIO.StringIO(test)
+        res = CSVParse(sio)
+        sio.close()
+        self.assertEqual(res, expectedRes)
+
+    def test_multiline(self):
+        test = "one,two,three\n1,2,3\n4,5,\"test\ntest\""
+        expectedRes = [{"one":"1","two":"2","three":"3"},{"one":"4","two":"5","three":"test\ntest"}]
         sio = StringIO.StringIO(test)
         res = CSVParse(sio)
         sio.close()
