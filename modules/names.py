5e22de7ffee4a571c115b1fcbf46a81a1179dab4
MonkIncr <monk.incr@gmail.com>
2012-02-10 16:14:27 +0500

    Issue 5 done. Функция выделения имени и фамилии из строки

--- modules/names.py
+++ modules/names.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import re
+
+gp_find = re.compile(r'\w+', re.LOCALE)
+
 def splitString(string):
     """
     Используется для выделения имени и фамилии из данной строки.
     Отбрасываются лишние пробелы, берутся первое и последнее слова в строке.
     На выходе tuple из двух строк: имени и фамилии.
     """
-    raise NotImplementedError
+    ll_res = gp_find.findall(string)
+    lv_res_len = len(ll_res)
+    
+    if lv_res_len > 1:
+	lt_out = (ll_res[0], ll_res[-1])
+    elif lv_res_len == 1:
+	lt_out = (ll_res[0], '')
+    else:
+	lt_out = ('', '')
+	
+    return lt_out
 
 ##############
 # unit tests #
